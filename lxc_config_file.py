cc12011cd01557be6882502f88482b428307d8b9
Nick Sklavenitis <sklav@sklav.com>
2014-05-05 21:01:11 -0400

    cleaned up lxc_config and lxc_inittab but stiff broken when it comes to generating the config

--- lxc_config_file.py
+++ lxc_config_file.py
@@ -1,44 +1,46 @@
 ###########################################
-#FUNCTIONS#
-def lxc_config_file():
-
+def lxc_config_file(fqdn, ram):
     "This generates a generic lxc config for containers"
-    config_file = open('/var/lib/lxc/'+args.fqdn+'/config','a')
-    print >>config_file, "lxc.utsname = ", args.fqdn
-    print >>config_file, "lxc.network.type = veth"
-    print >>config_file, "lxc.network.flags = up"
-    print >>config_file, "lxc.network.link = br0"
-    print >>config_file, "#lxc.network.hwaddr = xx:xx:xx:xx:xx:xx"
-    print >>config_file, "#lxc.network.ipv4 = 192.168.1.x"
-    print >>config_file, "#lxc.network.veth.pair = veth-"+args.fqdn
-    print >>config_file, "lxc.tty = 1"
-    print >>config_file, "lxc.pts = 1024"
-    print >>config_file, "#Memory"
-    print >>config_file, "lxc.cgroup.memory.limit_in_bytes = " +args.ram+"M"
-    print >>config_file, "lxc.cgroup.memory.memsw.limit_in_bytes = "+args.ram+"M"
-    print >>config_file, "lxc.cgroup.blkio.weight = 100"
-    print >>config_file, "# Lxc isolation"
-    print >>config_file, "#lxc.cap.drop = sys_module mac_admin mac_override sys_time sys_admin"
-    print >>config_file, "lxc.rootfs = /var/lib/lxc/"+args.fqdn+"/rootfs"
-    print >>config_file, "lxc.cgroup.devices.deny = a"
-    print >>config_file, "# /dev/null and zero"
-    print >>config_file, "lxc.cgroup.devices.allow = c 1:3 rwm"
-    print >>config_file, "lxc.cgroup.devices.allow = c 1:5 rwm"
-    print >>config_file, "# consoles"
-    print >>config_file, "lxc.cgroup.devices.allow = c 5:1 rwm"
-    print >>config_file, "lxc.cgroup.devices.allow = c 5:0 rwm"
-    print >>config_file, "lxc.cgroup.devices.allow = c 4:0 rwm"
-    print >>config_file, "lxc.cgroup.devices.allow = c 4:1 rwm"
-    print >>config_file, "# /dev/{,u}random"
-    print >>config_file, "lxc.cgroup.devices.allow = c 1:9 rwm"
-    print >>config_file, "lxc.cgroup.devices.allow = c 1:8 rwm"
-    print >>config_file, "lxc.cgroup.devices.allow = c 136:* rwm"
-    print >>config_file, "lxc.cgroup.devices.allow = c 5:2 rwm"
-    print >>config_file, "# rtc"
-    print >>config_file, "lxc.cgroup.devices.allow = c 254:0 rwm"
-    print >>config_file, "# mounts point"
-    print >>config_file, "lxc.mount.entry=proc /var/lib/lxc/"+args.fqdn+"/rootfs/proc proc nodev,noexec,nosuid 0 0"
-    print >>config_file, "lxc.mount.entry=devpts /var/lib/lxc/"+args.fqdn+"/rootfs/dev/pts devpts defaults 0 0"
-    print >>config_file, "lxc.mount.entry=sysfs /var/lib/lxc/"+args.fqdn+"/rootfs/sys sysfs defaults  0 0"
-    print >>config_file, "lxc.mount.entry=/etc/hostname /var/lib/lxc/"+args.fqdn+"/rootfs/etc/host none ro,bind 0 0"
-    config_file.close()
+    filename = '/var/lib/lxc/{fqdn}/config'.format(fqdn=fqdn)
+    with open(filename, 'a') as f:
+        f.write(lxc_config.format(fqdn=fqdn, ram=ram))
+
+lxc_config = """
+    lxc.utsname = fqdn
+    lxc.network.type = veth
+    lxc.network.flags = up
+    lxc.network.link = br0
+    #lxc.network.hwaddr = xx:xx:xx:xx:xx:xx
+    #lxc.network.ipv4 = 192.168.1.x
+    #lxc.network.veth.pair = veth-{fqdn}
+    lxc.tty = 1
+    lxc.pts = 1024
+    #Memory
+    lxc.cgroup.memory.limit_in_bytes = {ram}M
+    lxc.cgroup.memory.memsw.limit_in_bytes = {ram}M
+    lxc.cgroup.blkio.weight = 100
+    # Lxc isolation
+    #lxc.cap.drop = sys_module mac_admin mac_override sys_time sys_admin
+    lxc.rootfs = /var/lib/lxc/{fqdn}/rootfs
+    lxc.cgroup.devices.deny = a
+    # /dev/null and zero
+    lxc.cgroup.devices.allow = c 1:3 rwm
+    lxc.cgroup.devices.allow = c 1:5 rwm
+    # consoles
+    lxc.cgroup.devices.allow = c 5:1 rwm
+    lxc.cgroup.devices.allow = c 5:0 rwm
+    lxc.cgroup.devices.allow = c 4:0 rwm
+    lxc.cgroup.devices.allow = c 4:1 rwm
+    # /dev/{,u}random
+    lxc.cgroup.devices.allow = c 1:9 rwm
+    lxc.cgroup.devices.allow = c 1:8 rwm
+    lxc.cgroup.devices.allow = c 136:* rwm
+    lxc.cgroup.devices.allow = c 5:2 rwm
+    # rtc
+    lxc.cgroup.devices.allow = c 254:0 rwm
+    # mounts point
+    lxc.mount.entry=proc /var/lib/lxc/{fqdn}/rootfs/proc proc nodev,noexec,nosuid 0 0
+    lxc.mount.entry=devpts /var/lib/lxc/{fqdn}/rootfs/dev/pts devpts defaults 0 0
+    lxc.mount.entry=sysfs /var/lib/lxc/{fqdn}/rootfs/sys sysfs defaults  0 0
+    lxc.mount.entry=/etc/hostname /var/lib/lxc/{fqdn}/rootfs/etc/host none ro,bind 0 0
+    """
